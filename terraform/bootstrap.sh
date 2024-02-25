7cd50e7d69384aacf02d0ee5d4774296b33f05fc
jaga319 <mummadijagadeesh219@gmail.com>
2024-02-25 22:55:17 +0530

    add

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 component=$1
 environment=$2 
-environment=$3
+app_version=$3
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botcore boto3
-ansible-pull -U https://github.com/jaga319/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app-version=$app_version main-tf.yaml
+ansible-pull -U https://github.com/jaga319/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
