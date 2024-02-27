1834b6fb5f7146f41fc1b11c5f9d7445e81d849a
jaga319 <mummadijagadeesh219@gmail.com>
2024-02-28 02:40:51 +0530

    app adding

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 module "catalogue" {
     source = "git::https://github.com/jaga319/terraform-roboshop-app.git.git?ref=main"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    Component_sg_id = data.aws_ssm_parameter.user_sg_id.value
+    Component_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
-    iam_instance_profile = var.iam_instance_profile
+    # iam_instance_profile = var.iam_instance_profile
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
